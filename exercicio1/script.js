f54a9280abd7712ff3b3ea9cf897a412321be462
Amanda <amandaoliveirapolari@gmail.com>
2023-02-02 11:26:23 -0300

    Exercício 1 resolvido!

---
+++ exercicio1/script.js
@@ -0,0 +1,9 @@
+const numeroInseridoPeloUsario = Number(prompt('Por favor, insira um número par:'));
+
+const restoDaDivisaoPorDois = numeroInseridoPeloUsario % 2;
+
+console.log(`O resto da divisão por dois é: ${restoDaDivisaoPorDois}`);
+
+// Qualquer número par dividido por 2 possui resto 0;
+
+// Qualquer número ímpar dividido por 2 possui resto 1;
